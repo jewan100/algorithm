a2e713df6a74c76ad9632fb803b35b8b4f09d7af
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2025-07-10 09:13:07 +0900

    [Silver IV] Title: 등수 구하기, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		int n = read(), s = read(), p = read();
+		ArrayList<Integer> al = new ArrayList<>();
+		for (int i = 0; i < n; i++)
+			al.add(read());
+		al.sort((o1, o2) -> o2 - o1);
+		int rank = 1;
+		for (int i = 0; i < n; i++) {
+			if (al.get(i) > s)
+				rank++;
+			else if (al.get(i) < s)
+				break;
+			p--;
+		}
+		if (p <= 0)
+			rank = -1;
+		System.out.println(rank);
+	}
+
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
+}
