8ef070478df7ad86b7dd16a7f043cb740ed431f9
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2025-08-09 23:34:43 +0900

    [Bronze IV] Title: X に最も近い値 (The Nearest Value), Time: 108 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/18414. X に最も近い値 （The Nearest Value）/X に最も近い値 （The Nearest Value）.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+        int min = 100001;
+        for (int i = l; i <= r; i++) {
+            if (min > Math.abs(x - i)) {
+                min = Math.abs(x - i);
+                result = i;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
