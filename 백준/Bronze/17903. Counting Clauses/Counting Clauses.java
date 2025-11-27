c43b2a30b91fd90c0ee722ae788cf35a5e4aec3c
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2025-11-27 23:47:41 +0900

    [Bronze IV] Title: Counting Clauses, Time: 180 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		
+		if(m >= 8) {
+			System.out.println("satisfactory");
+		}else {
+			System.out.println("unsatisfactory");
+		}
+		sc.close();
+	}
+}
