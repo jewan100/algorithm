bf089032d8ce3cf2c13c4b7a6662de9da5aeccc6
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2025-12-26 23:08:15 +0900

    [Bronze III] Title: 거울, 오! 거울, Time: 104 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/4740. 거울， 오！ 거울/거울， 오！ 거울.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String s = br.readLine();
+
+            if (s.equals("***")) {
+                break;
+            }
+
+            StringBuffer sb = new StringBuffer(s);
+            String reverse = sb.reverse().toString();
+
+            System.out.println(reverse);
+
+        }
+    }
+}
