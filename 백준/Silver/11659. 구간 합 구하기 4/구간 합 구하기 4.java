3ed9c890c82daee8e9bbf71878740323c70b231e
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-11 18:38:40 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 260 ms, Memory: 20640 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,32 +1,27 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int[] arr = new int[N + 1];
-        long[] dp = new long[N + 1];
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = dp[i - 1] + arr[i];
-        }
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (; M > 0; M--) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            bw.write(dp[b] - dp[a - 1] + "\n");
-        }
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+
+	public static void main(String[] args) throws Exception {
+		int n = read(), m = read();
+		int[] arr = new int[n];
+		int[] dp = new int[n + 1];
+		for (int i = 0; i < n; i++) {
+			arr[i] = read();
+			dp[i + 1] = dp[i] + arr[i];
+		}
+		StringBuilder sb = new StringBuilder();
+		while (m-- > 0) {
+			int i = read(), j = read();
+			sb.append(dp[j] - dp[i - 1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
 }
