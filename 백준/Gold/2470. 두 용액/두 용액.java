ca9b5eb466bc806c41953b63c96d2a76bbbef7a6
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-11 18:16:20 +0900

    [Gold V] Title: 두 용액, Time: 164 ms, Memory: 18844 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -2,37 +2,44 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        int n = read();
-        int[] arr = new int[n];
-        int max = 2_000_000_001;
-        int[] result = new int[2];
-        for (int i = 0; i < n; i++)
-            arr[i] = read();
-        Arrays.sort(arr);
-        int l = 0, r = n - 1;
-        while (l < r) {
-            int sum = arr[l] + arr[r];
-            int dist = Math.abs(sum);
-            if (max > dist) {
-                max = dist;
-                result = new int[] { arr[l], arr[r] };
-            }
-            if (sum > 0)
-                r--;
-            else
-                l++;
-        }
-        System.out.println(result[0] + " " + result[1]);
-    }
+	static int n;
+	static int[] arr;
 
-    private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        boolean m = n == 13;
-        if (m)
-            n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48)
-            n = (n << 3) + (n << 1) + (c & 15);
-        return m ? ~n + 1 : n;
-    }
+	public static void main(String[] args) throws Exception {
+		n = read();
+		arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = read();
+		Arrays.sort(arr);
+		int[] result = new int[2];
+		int minDiff = 1_000_000_001 << 1;
+		int l = 0, r = n - 1;
+		while (l < r) {
+			int sum = arr[l] + arr[r];
+			int diff = Math.abs(sum);
+			if (diff < minDiff) {
+				minDiff = diff;
+				result = new int[]{arr[l], arr[r]};
+			}
+			if (diff == 0)
+				break;
+			if (sum < 0)
+				l++;
+			else
+				r--;
+		}
+		System.out.println(result[0] + " " + result[1]);
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean m = n == 13;
+		if (m)
+			n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return m ? ~n + 1 : n;
+	}
 }
