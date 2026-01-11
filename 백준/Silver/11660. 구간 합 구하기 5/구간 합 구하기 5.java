3c5eff377b7cb2a6ae8bde7b877b22fe9eacdb98
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-11 18:56:56 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 340 ms, Memory: 23744 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,38 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] board = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken()) + board[i][j - 1];
-            }
-        }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                board[i][j] += board[i - 1][j];
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int sum = board[x2][y2] + board[x1 - 1][y1 - 1] - board[x2][y1 - 1] - board[x1 - 1][y2];
-            sb.append(sum).append("\n");
-        }
-        System.out.println(sb);
-        br.close();
-    }
+	public static void main(String[] args) throws Exception {
+		int n = read(), m = read();
+		int[][] dp = new int[n + 1][n + 1];
+		for (int i = 1; i < n + 1; i++)
+			for (int j = 1; j < n + 1; j++)
+				dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + read();
+		StringBuilder sb = new StringBuilder();
+		while (m-- > 0) {
+			int x1 = read(), y1 = read(), x2 = read(), y2 = read();
+			sb.append(dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
 }
