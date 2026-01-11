64b54b28cbb3f7cda656a76eb31bdaf68430d6ff
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-11 19:44:49 +0900

    [Silver II] Title: DFS와 BFS, Time: 160 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,76 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (; m > 0; m--) {
-            st = new StringTokenizer(br.readLine());
-            int v = Integer.parseInt(st.nextToken());
-            int u = Integer.parseInt(st.nextToken());
-            graph.get(v).add(u);
-            graph.get(u).add(v);
-        }
-        for (int i = 0; i <= n; i++) {
-            graph.get(i).sort((o1, o2) -> o2 - o1);
-        }
-        dfs(graph, s, new boolean[n + 1]);
-        for (int i = 0; i <= n; i++) {
-            graph.get(i).sort((o1, o2) -> o1 - o2);
-        }
-        bfs(graph, s, new boolean[n + 1]);
-        System.out.println();
-    }
 
-    public static void dfs(ArrayList<ArrayList<Integer>> graph, int s, boolean[] visited) {
-        Stack<Integer> stk = new Stack<>();
-        stk.push(s);
-        StringBuilder sb = new StringBuilder();
-        while (!stk.isEmpty()) {
-            int cur = stk.pop();
-            if (visited[cur])
-                continue;
-            visited[cur] = true;
-            sb.append(cur).append(" ");
-            for (int i : graph.get(cur)) {
-                if (!visited[i]) {
-                    stk.push(i);
-                }
-            }
-        }
-        System.out.println(sb.toString());
-    }
+	static int n, m, v;
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	static StringBuilder sb = new StringBuilder();
 
-    public static void bfs(ArrayList<ArrayList<Integer>> graph, int s, boolean[] visited) {
-        Deque<Integer> deq = new ArrayDeque<>();
-        deq.add(s);
-        StringBuilder sb = new StringBuilder();
-        while (!deq.isEmpty()) {
-            int cur = deq.poll();
-            if (visited[cur])
-                continue;
-            visited[cur] = true;
-            sb.append(cur).append(" ");
-            for (int i : graph.get(cur)) {
-                if (!visited[i]) {
-                    deq.add(i);
-                }
-            }
-        }
-        System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws Exception {
+		n = read();
+		m = read();
+		v = read();
+		for (int i = 0; i < n + 1; i++)
+			graph.add(new ArrayList<>());
+		for (int i = 0; i < m; i++) {
+			int u = read(), v = read();
+			graph.get(u).add(v);
+			graph.get(v).add(u);
+		}
+		for (int i = 1; i < n + 1; i++)
+			graph.get(i).sort((o1, o2) -> o1 - o2);
+		dfs(v, new boolean[n + 1]);
+		sb.append("\n");
+		bfs(v, new boolean[n + 1]);
+		System.out.println(sb);
+	}
+
+	private static void dfs(int u, boolean[] visited) {
+		sb.append(u).append(" ");
+		visited[u] = true;
+		for (int v : graph.get(u))
+			if (!visited[v])
+				dfs(v, visited);
+	}
+
+	private static void bfs(int u, boolean[] visited) {
+		Deque<Integer> dq = new ArrayDeque<>();
+		dq.offer(u);
+		visited[u] = true;
+		while (!dq.isEmpty()) {
+			int cur = dq.poll();
+			sb.append(cur).append(" ");
+			for (int v : graph.get(cur)) {
+				if (!visited[v]) {
+					dq.offer(v);
+					visited[v] = true;
+				}
+			}
+		}
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
 }
