50b0b047b392bd3e659e185785a1ad31fc15707f
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-12 00:37:47 +0900

    [Gold III] Title: 줄 세우기, Time: 232 ms, Memory: 22452 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -4,38 +4,38 @@ import java.util.Deque;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        int n = read(), m = read();
-        ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
-        int[] indegree = new int[n + 1];
-        for (int i = 0; i < n + 1; i++)
-            edges.add(new ArrayList<>());
-        while (m-- > 0) {
-            int s = read(), e = read();
-            indegree[e]++;
-            edges.get(s).add(e);
-        }
-        StringBuilder sb = new StringBuilder();
-        Deque<Integer> dq = new ArrayDeque<>();
-        for (int i = 1; i < n + 1; i++)
-            if (indegree[i] == 0)
-                dq.offer(i);
-        while (!dq.isEmpty()) {
-            int cur = dq.poll();
-            sb.append(cur).append(" ");
-            for (int v : edges.get(cur)) {
-                indegree[v]--;
-                if (indegree[v] == 0)
-                    dq.offer(v);
-            }
-        }
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws Exception {
+		int n = read(), m = read();
+		int[] indegree = new int[n + 1];
+		ArrayList<ArrayList<Integer>> al = new ArrayList<>();
+		for (int i = 0; i < n + 1; i++)
+			al.add(new ArrayList<>());
+		for (int i = 0; i < m; i++) {
+			int u = read(), v = read();
+			indegree[v]++;
+			al.get(u).add(v);
+		}
+		StringBuilder sb = new StringBuilder();
+		Deque<Integer> dq = new ArrayDeque<>();
+		for (int i = 1; i < n + 1; i++)
+			if (indegree[i] == 0)
+				dq.offer(i);
+		while (!dq.isEmpty()) {
+			int u = dq.poll();
+			sb.append(u).append(" ");
+			for (int v : al.get(u))
+				if (--indegree[v] == 0)
+					dq.offer(v);
+		}
+		System.out.println(sb);
+	}
 
-    private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48)
-            n = (n << 3) + (n << 1) + (c & 15);
-        return n;
-    }
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
 }
