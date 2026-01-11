d301d24d30609f99ba2b101fe9aafbb7a24eb405
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-11 19:33:08 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -7,39 +6,50 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] board = new int[n][m];
-        int[] dy = {1, -1, 0, 0};
-        int[] dx = {0, 0, 1, -1};
-        for (int i = 0; i < n; i++) {
-            String[] strArr = br.readLine().split("");
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(strArr[j]);
-            }
-        }
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.add(new int[] {0, 0});
-        while (!dq.isEmpty()) {
-            int[] cur = dq.poll();
-            if (cur[0] == n - 1 && cur[1] == m - 1)
-                break;
-            int v = board[cur[0]][cur[1]];
-            board[cur[0]][cur[1]] = 0;
-            for (int i = 0; i < 4; i++) {
-                int y = cur[0] + dy[i];
-                int x = cur[1] + dx[i];
-                if (0 <= y && y < n && 0 <= x && x < m) {
-                    if (board[y][x] == 1) {
-                        board[y][x] = v + 1;
-                        dq.add(new int[] {y, x});
-                    }
-                }
-            }
-        }
-        System.out.println(board[n - 1][m - 1]);
-    }
+	static int n, m;
+	static int[] dy = {1, -1, 0, 0}, dx = {0, 0, 1, -1};
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		boolean[][] board = new boolean[n][m];
+		for (int i = 0; i < n; i++) {
+			char[] cArr = br.readLine().toCharArray();
+			for (int j = 0; j < m; j++)
+				board[i][j] = cArr[j] == '1';
+		}
+		Deque<int[]> dq = new ArrayDeque<>();
+		dq.offer(new int[]{0, 0, 1});
+		board[0][0] = false;
+		while (!dq.isEmpty()) {
+			int[] cur = dq.poll();
+			int y = cur[0], x = cur[1], t = cur[2];
+			if (y == n - 1 && x == m - 1) {
+				System.out.println(t);
+				return;
+			}
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dy[i], nx = x + dx[i];
+				if (isValid(ny, nx) && board[ny][nx]) {
+					board[ny][nx] = false;
+					dq.offer(new int[]{ny, nx, t + 1});
+				}
+			}
+		}
+	}
+
+	private static boolean isValid(int y, int x) {
+		return 0 <= y && y < n && 0 <= x && x < m;
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
 }
