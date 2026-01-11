dc3a46fdd6bfaf4a9eca3946b3c8923b8a734200
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-11 19:18:47 +0900

    [Gold IV] Title: 오큰수, Time: 592 ms, Memory: 76232 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -3,31 +3,31 @@ import java.util.Deque;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        int n = read();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++)
-            arr[i] = read();
-        Deque<Integer> dq = new ArrayDeque<>();
-        int[] answer = new int[n];
-        for (int i = n - 1; i >= 0; i--) {
-            while (!dq.isEmpty() && dq.peekLast() <= arr[i])
-                dq.pollLast();
-            answer[i] = dq.isEmpty() ? -1 : dq.peekLast();
-            dq.offerLast(arr[i]);
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i : answer)
-            sb.append(i).append(" ");
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws Exception {
+		int n = read();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = read();
+		int[] answer = new int[n];
+		Deque<Integer> dq = new ArrayDeque<>();
+		for (int i = n - 1; i >= 0; i--) {
+			while (!dq.isEmpty() && dq.peek() <= arr[i])
+				dq.poll();
+			answer[i] = dq.isEmpty() ? -1 : dq.peek();
+			dq.offerFirst(arr[i]);
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i : answer)
+			sb.append(i).append(" ");
+		System.out.println(sb);
+	}
 
-    private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48)
-            n = (n << 3) + (n << 1) + (c & 15);
-        if (c == 13)
-            System.in.read();
-        return n;
-    }
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
 }
