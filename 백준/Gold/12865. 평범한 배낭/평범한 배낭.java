d0490a47d54917d0fa2f90ed7a3bdea26d23ed40
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-12 11:35:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 128 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,30 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[][] item = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            item[i] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
-        }
-        int[][] dp = new int[n + 1][k + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= k; j++) {
-                if (item[i][0] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - item[i][0]] + item[i][1]);
-                }
-            }
-        }
-        System.out.println(dp[n][k]);
-        br.close();
-    }
+
+	static int n, k;
+
+	public static void main(String[] args) throws Exception {
+		n = read();
+		k = read();
+		int[] dp = new int[k + 1];
+		for (int i = 0; i < n; i++) {
+			int w = read(), v = read();
+			for (int j = k - w; j >= 0; j--)
+				dp[j + w] = Math.max(dp[j + w], dp[j] + v);
+		}
+		System.out.println(dp[k]);
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
 }
