2de484562d1a4ba83a229fa0add2a1e4645c6389
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-12 11:42:43 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 312 ms, Memory: 22416 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -2,30 +2,31 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        int n = read();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++)
-            arr[i] = read();
-        int[] lis = new int[n];
-        int len = 0;
-        for (int i = 0; i < n; i++) {
-            int pos = Arrays.binarySearch(lis, 0, len, arr[i]);
-            if (pos < 0)
-                pos = ~pos;
-            lis[pos] = arr[i];
-            if (pos == len)
-                len++;
-        }
-        System.out.println(len);
-    }
+	public static void main(String[] args) throws Exception {
+		int n = read();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = read();
+		int[] lis = new int[n];
+		int len = 0;
+		for (int i = 0; i < n; i++) {
+			int idx = Arrays.binarySearch(lis, 0, len, arr[i]);
+			if (idx < 0) {
+				idx = ~idx;
+			}
+			if (idx == len)
+				len++;
+			lis[idx] = arr[i];
+		}
+		System.out.println(len);
+	}
 
-    private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48)
-            n = (n << 3) + (n << 1) + (c & 15);
-        if (c == 13)
-            System.in.read();
-        return n;
-    }
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
 }
