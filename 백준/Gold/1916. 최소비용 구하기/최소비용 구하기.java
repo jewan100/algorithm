c3c19a0d3efe8314c9fbe3a0d03d3d9da59d4eac
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-31 23:11:10 +0900

    [Gold V] Title: 최소비용 구하기, Time: 432 ms, Memory: 49900 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,65 +1,47 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static class Edge {
-        int to;
-        int weight;
+	static final int MAX = Integer.MAX_VALUE >> 2;
 
-        public Edge(int to, int weight) {
-            this.to = to;
-            this.weight = weight;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        List<List<Edge>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        StringTokenizer st;
-        for (; m > 0; m--) {
-            st = new StringTokenizer(br.readLine());
-            int v = Integer.parseInt(st.nextToken());
-            int u = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph.get(v).add(new Edge(u, w));
-        }
-        st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        int[] dist = new int[n + 1];
-        boolean[] visited = new boolean[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[s] = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((v1, v2) -> dist[v1] - dist[v2]);
-        pq.offer(s);
-        while (!pq.isEmpty()) {
-            int u = pq.poll();
-            if (visited[u]) {
-                continue;
-            }
-            visited[u] = true;
-            for (Edge edge : graph.get(u)) {
-                int v = edge.to;
-                int w = edge.weight;
-                if (dist[u] + w < dist[v]) {
-                    dist[v] = dist[u] + w;
-                    pq.offer(v);
-                }
-            }
-        }
-        System.out.println(dist[e]);
-        br.close();
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine()), m = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+		for (int i = 0; i < n + 1; i++)
+			graph.add(new ArrayList<>());
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken()), v = Integer.parseInt(st.nextToken()),
+					w = Integer.parseInt(st.nextToken());
+			graph.get(u).add(new int[]{v, w});
+		}
+		st = new StringTokenizer(br.readLine());
+		int s = Integer.parseInt(st.nextToken()), e = Integer.parseInt(st.nextToken());
+		int[] dist = new int[n + 1];
+		Arrays.fill(dist, MAX);
+		dist[s] = 0;
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> dist[o1] - dist[o2]);
+		boolean[] visited = new boolean[n + 1];
+		pq.offer(s);
+		while (!pq.isEmpty()) {
+			int cur = pq.poll();
+			if (visited[cur])
+				continue;
+			visited[cur] = true;
+			for (int[] next : graph.get(cur)) {
+				if (dist[next[0]] > dist[cur] + next[1]) {
+					dist[next[0]] = dist[cur] + next[1];
+					pq.offer(next[0]);
+				}
+			}
+		}
+		System.out.println(dist[e]);
+	}
 }
