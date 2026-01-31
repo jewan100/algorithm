009a11947fa6d6a472666fa2173eb8e78a6d83fc
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-31 16:51:45 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 240 ms, Memory: 14976 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,34 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        int n = read();
-        int k = read();
-        Deque<Integer> dq = new ArrayDeque<>();
-        for (int i = 1; i <= n; i++) {
-            dq.offer(i);
-        }
-        StringBuilder sb = new StringBuilder("<");
-        int cnt = 1;
-        while (!dq.isEmpty()) {
-            if (cnt % k == 0) {
-                sb.append(dq.poll()).append(", ");
-            } else {
-                dq.offer(dq.poll());
-            }
-            cnt++;
-        }
-        sb.replace(sb.length()-2, sb.length(), ">");
-        System.out.println(sb);
-    }
 
-    private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48)
-            n = (n << 3) + (n << 1) + (c & 15);
-        if (c == 13)
-            System.in.read();
-        return n;
-    }
+	static int n, k;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder("<");
+		Deque<Integer> dq = new ArrayDeque<>();
+		for (int i = 1; i < n + 1; i++)
+			dq.offer(i);
+		while (!dq.isEmpty()) {
+			for (int i = 0; i < k - 1; i++)
+				dq.offer(dq.poll());
+			sb.append(dq.poll());
+			if (!dq.isEmpty())
+				sb.append(", ");
+		}
+		System.out.println(sb.append(">"));
+	}
 }
