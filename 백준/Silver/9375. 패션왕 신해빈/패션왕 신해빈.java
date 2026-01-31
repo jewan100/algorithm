eb39d0fed7a8df8b6d54c9e1ea0177585eda75a1
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-01-31 17:55:46 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,30 +1,26 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<String, Integer> hm;
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        for (; n > 0; n--) {
-            int m = Integer.parseInt(br.readLine());
-            hm = new HashMap<>();
-            for (; m > 0; m--) {
-                st = new StringTokenizer(br.readLine());
-                st.nextToken();
-                String cate = st.nextToken();
-                hm.putIfAbsent(cate, 1);
-                hm.replace(cate, hm.get(cate) + 1);
-            }
-            int cnt = 1;
-            for (String cate : hm.keySet()) {
-                cnt *= hm.get(cate);
-            }
-            System.out.println(cnt - 1);
-        }
-    }
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			HashMap<String, Integer> hm = new HashMap<>();
+			for (int i = 0; i < n; i++) {
+				String[] sArr = br.readLine().split(" ");
+				hm.putIfAbsent(sArr[1], 0);
+				hm.put(sArr[1], hm.get(sArr[1]) + 1);
+			}
+			int answer = 1;
+			for (String type : hm.keySet())
+				answer *= hm.get(type) + 1;
+			sb.append(answer - 1).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
