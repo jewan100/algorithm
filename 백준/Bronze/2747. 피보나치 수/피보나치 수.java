1a675e4087c4caa5c6e137a578396f1a045b4467
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2026-03-14 22:38:47 +0900

    [Bronze II] Title: 피보나치 수, Time: 96 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int A = 0;
+		int B = 1;
+		
+		for(int i = 2; i <= N; i++) {
+			if(i % 2 == 0) {
+				A += B;
+			}else {
+				B += A;
+			}
+		}
+		if(N % 2 == 0) {
+			System.out.println(A);
+		}else {
+			System.out.println(B);
+		}
+	}
+
+}
